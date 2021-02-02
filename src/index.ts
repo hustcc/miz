8032448a8f90c5119310d8788f64580e36259048
hustcc <i@hust.cc>
2021-02-02 11:20:02 +0800

    chore: avoid export * as x

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@
  * Contract: i@hust.cc
  */
 
-export * as M from './mockers';
+import { apply, arrayOf, bool, constant, number, oneOf, shape, string } from  './mockers';
 
 export { randomBool, randomChar, randomFloat, randomString } from './mockers/random';
 export { Mocker } from './mocker';
+export const M = { apply, arrayOf, bool, constant, number, oneOf, shape, string };
